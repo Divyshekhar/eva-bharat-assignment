37e54e70aa359531f89d8df900db7ac382a51d46
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2026-07-04 12:54:45 +0530

    user repository

---
+++ internal/repository/user_repository.go
@@ -0,0 +1,46 @@
+package repository
+
+import (
+	"github.com/Divyshekhar/eva-bharat-assignment/internal/models"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	Create(user *models.User) error
+	GetByEmail(email string) (*models.User, error)
+	GetByID(id uuid.UUID) (*models.User, error)
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func (r *userRepository) Create(user *models.User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *userRepository) GetByEmail(email string) (*models.User, error) {
+	var user models.User
+
+	err := r.db.Where("email = ?", email).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+func (r *userRepository) GetByID(id uuid.UUID) (*models.User, error) {
+	var user models.User
+
+	err := r.db.First(&user, "id = ?", id).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{db: db}
+}
